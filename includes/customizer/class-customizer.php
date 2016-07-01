9859aea0952aed16c56138ae6eeeb44b9c35e603
Thomas W <netzberufler@googlemail.com>
2016-07-01 12:33:53 +0200

    added header spacing and completed mobile navigation

--- includes/customizer/class-customizer.php
+++ includes/customizer/class-customizer.php
@@ -22,41 +22,41 @@ class Admiral_Pro_Customizer {
 	 * @return void
 	*/
 	static function setup() {
-		
+
 		// Return early if Admiral Theme is not active
 		if ( ! current_theme_supports( 'admiral-pro'  ) ) {
 			return;
 		}
-		
+
 		// Enqueue scripts and styles in the Customizer
 		add_action( 'customize_preview_init', array( __CLASS__, 'customize_preview_js' ) );
 		add_action( 'customize_controls_print_styles', array( __CLASS__, 'customize_preview_css' ) );
-		
+
 		// Remove Upgrade section
 		remove_action( 'customize_register', 'admiral_customize_register_upgrade_settings' );
 	}
-	
+
 	/**
 	 * Get saved user settings from database or plugin defaults
 	 *
 	 * @return array
 	 */
 	static function get_theme_options() {
-		
+
 		// Merge Theme Options Array from Database with Default Options Array
-		$theme_options = wp_parse_args( 
-			
+		$theme_options = wp_parse_args(
+
 			// Get saved theme options from WP database
-			get_option( 'admiral_theme_options', array() ), 
-			
+			get_option( 'admiral_theme_options', array() ),
+
 			// Merge with Default Options if setting was not saved yet
-			self::get_default_options() 
-			
+			self::get_default_options()
+
 		);
 
 		// Return theme options
 		return $theme_options;
-		
+
 	}
 
 
@@ -69,7 +69,7 @@ class Admiral_Pro_Customizer {
 
 		$default_options = array(
 			'logo_spacing'						=> 0,
-			'header_spacing'					=> 20,
+			'header_spacing'					=> 30,
 			'footer_text'						=> '',
 			'credit_link' 						=> true,
 			'navi_primary_color'				=> '#25b5d5',
@@ -83,20 +83,20 @@ class Admiral_Pro_Customizer {
 			'widget_title_font' 				=> 'Roboto',
 			'available_fonts'					=> 'favorites'
 		);
-		
+
 		return $default_options;
-		
+
 	}
-	
+
 	/**
 	 * Embed JS file to make Theme Customizer preview reload changes asynchronously.
 	 *
 	 * @return void
 	 */
 	static function customize_preview_js() {
-		
+
 		wp_enqueue_script( 'admiral-pro-customizer-js', ADMIRAL_PRO_PLUGIN_URL . 'assets/js/customizer.js', array( 'customize-preview' ), ADMIRAL_PRO_VERSION, true );
-	
+
 	}
 
 	/**
@@ -105,9 +105,9 @@ class Admiral_Pro_Customizer {
 	 * @return void
 	 */
 	static function customize_preview_css() {
-		
+
 		wp_enqueue_style( 'admiral-pro-customizer-css', ADMIRAL_PRO_PLUGIN_URL . 'assets/css/customizer.css', array(), ADMIRAL_PRO_VERSION );
-	
+
 	}
 
 }
--- includes/modules/class-header-spacing.php
+++ includes/modules/class-header-spacing.php
@@ -22,49 +22,49 @@ class Admiral_Pro_Header_Spacing {
 	 * @return void
 	*/
 	static function setup() {
-		
+
 		// Return early if Admiral Theme is not active
 		if ( ! current_theme_supports( 'admiral-pro'  ) ) {
 			return;
 		}
-		
+
 		// Add Custom Spacing CSS code to custom stylesheet output
-		add_filter( 'admiral_pro_custom_css_stylesheet', array( __CLASS__, 'custom_spacing_css' ) ); 
-		
+		add_filter( 'admiral_pro_custom_css_stylesheet', array( __CLASS__, 'custom_spacing_css' ) );
+
 		// Add Header Spacing Settings
 		add_action( 'customize_register', array( __CLASS__, 'header_spacing_settings' ) );
 	}
-	
+
 	/**
 	 * Adds custom Margin CSS styling for the logo and navigation spacing
 	 *
 	 */
-	static function custom_spacing_css( $custom_css ) { 
-		
+	static function custom_spacing_css( $custom_css ) {
+
 		// Get Theme Options from Database
 		$theme_options = Admiral_Pro_Customizer::get_theme_options();
 
 		// Set CSS Variable
 		$spacing_css = '';
-		
+
 		// Set Logo Spacing
-		if ( $theme_options['logo_spacing'] <> 0 ) { 
-		
+		if ( $theme_options['logo_spacing'] <> 0 ) {
+
 			$margin = $theme_options['logo_spacing'] / 10;
-		
+
 			$spacing_css .= '
 				.site-branding {
 					margin: '. $margin .'em 0;
 				}
 				';
-				
+
 		}
-		
+
 		// Set Navigation Spacing
-		if ( $theme_options['header_spacing'] <> 20 ) { 
-		
+		if ( $theme_options['header_spacing'] <> 30 ) {
+
 			$margin = $theme_options['header_spacing'] / 10;
-		
+
 			$spacing_css .= '
 				@media only screen and (min-width: 60em) {
 
@@ -72,19 +72,19 @@ class Admiral_Pro_Header_Spacing {
 						padding-top: '. $margin .'em;
 						padding-bottom: '. $margin .'em;
 					}
-					
+
 				}
 				';
-				
+
 		}
-		
+
 		// Add Spacing CSS to existing CSS code
 		$custom_css .= $spacing_css;
-		
+
 		return $custom_css;
-		
+
 	}
-	
+
 	/**
 	 * Adds header spacing settings
 	 *
@@ -96,10 +96,10 @@ class Admiral_Pro_Header_Spacing {
 		$wp_customize->add_section( 'admiral_pro_section_header', array(
 			'title'    => __( 'Header Spacing', 'admiral-pro' ),
 			'priority' => 20,
-			'panel' => 'admiral_options_panel' 
+			'panel' => 'admiral_options_panel'
 			)
 		);
-		
+
 		// Add Logo Spacing setting
 		$wp_customize->add_setting( 'admiral_theme_options[logo_spacing]', array(
 			'default'           => 0,
@@ -116,17 +116,17 @@ class Admiral_Pro_Header_Spacing {
 			'priority' => 2
 			)
 		);
-		
+
 		// Add Header Spacing setting
 		$wp_customize->add_setting( 'admiral_theme_options[header_spacing]', array(
-			'default'           => 20,
+			'default'           => 30,
 			'type'           	=> 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'absint'
 			)
 		);
 		$wp_customize->add_control( 'admiral_theme_options[header_spacing]', array(
-			'label'    => __( 'Header Spacing (default: 20)', 'admiral-pro' ),
+			'label'    => __( 'Header Spacing (default: 30)', 'admiral-pro' ),
 			'section'  => 'admiral_pro_section_header',
 			'settings' => 'admiral_theme_options[header_spacing]',
 			'type'     => 'text',
